7f966bd5e5cead3510b65343c3b7da55251daea1
futurebound <mitchellm1618@gmail.com>
2025-01-20 14:33:02 -0800

    refactor: modularize routes

--- src/main.jsx
+++ src/main.jsx
@@ -1,21 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import App from './App'
-import Profile from './components/Profile'
-import ErrorPage from './components/ErrorPage'
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />,
-    errorElement: <ErrorPage />,
-  },
-  {
-    path: 'profile/:name',
-    element: <Profile />,
-  },
-])
+import routes from './routes'
+
+const router = createBrowserRouter(routes)
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
